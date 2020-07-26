06c9b0e1a01277df3e0d3a2a3c6a740b047c43ab
Alexey Khrushch <alexeykhr@outlook.com>
2020-07-26 21:55:29 +0300

    chore: add comments to config file

--- config/clickhouse.php
+++ config/clickhouse.php
@@ -1,6 +1,17 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | ClickHouse Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you can configure a connection to connect to the ClickHouse
+    | database and specify additional configurations
+    |
+    */
+
     'config' => [
         'host' => env('CLICKHOUSE_HOST', 'localhost'),
         'port' => env('CLICKHOUSE_PORT', 8123),
@@ -12,6 +23,15 @@ return [
             'connectTimeOut' => 2,
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | ClickHouse Migrations
+    |--------------------------------------------------------------------------
+    |
+    | ClickHouse settings for working with migrations
+    |
+    */
 
     'migrations' => [
         'table' => env('CLICKHOUSE_MIGRATION_TABLE_NAME', 'migrations'),
